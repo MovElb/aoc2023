7c2ad73308313e9dd66bac8fb45d2a8abe715a51
MovElb <maelbakian@gmail.com>
2023-12-01 22:47:23 +0300

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (l in input) {
+            sum += (l.find { it.isDigit() }!!.toString() + l.findLast { it.isDigit() }!!.toString()).toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val strToDig = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+        )
+
+        val allNums = strToDig.keys + (1..9).map { it.toString() }.toSet()
+
+        val toDigit = { digStr: String ->
+            digStr.toIntOrNull() ?: strToDig[digStr]!!
+        }
+
+        var sum = 0
+        for (l in input) {
+            sum += toDigit(l.findAnyOf(allNums, ignoreCase = true)!!.second) * 10 + toDigit(l.findLastAnyOf(allNums, ignoreCase = true)!!.second)
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test_2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
